7764c1e6cfb65dcf8c8197c2bde8fc59dc847ebc
Anderson <andersonsalles@outlook.com>
2020-07-11 16:31:23 -0300

    add an Interface with basics auth methods

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasicLogin.Models;
+
+namespace BasicLogin.Data
+{
+    public interface IAuthRepository
+    {
+        Task<User> Register(User user, string Password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+
+    }
+}
