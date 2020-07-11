97ac5edaf528089f4ea7e9be15ec62b5e2bf0aeb
Anderson <andersonsalles@outlook.com>
2020-07-11 15:51:22 -0300

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using BasicLogin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicLogin.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
