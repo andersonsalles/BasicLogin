496f4c26071e692878537562fc2c8565354fbb9b
Anderson <andersonsalles@outlook.com>
2020-07-11 19:53:12 -0300

    add authentication tests

---
+++ Controllers/TestAuthController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicLogin.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAuthController : ControllerBase
+    {
+        [HttpGet("auth")]
+        public IActionResult Auth()
+        {
+            return Ok("This is an access authorized");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("anonymousTest")]
+        public IActionResult AnonymousTest()
+        {
+            return Ok("This is an access anonymous");
+        }
+    }
+}
