774b10bd717448558dc22dd6650bf89fbfa4769b
Anderson <andersonsalles@outlook.com>
2020-07-11 16:48:24 -0300

    create register method

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BasicLogin.Models;
+
+namespace BasicLogin.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Register(User user, string Password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(Password, out passwordHash, out passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
