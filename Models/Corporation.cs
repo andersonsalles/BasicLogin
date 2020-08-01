1579c76dd78a47c034872619ea4493329af7f952
Anderson <andersonsalles@outlook.com>
2020-08-01 18:17:04 -0300

    add new tables related with person

---
+++ Models/Corporation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace BasicLogin.Models
+{
+    public class Corporation
+    {
+        [Required]
+        public string Cnpj { get; set; }
+        public string Ie { get; set; }
+        public string RazaoSocial { get; set; }
+        
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicLogin.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
